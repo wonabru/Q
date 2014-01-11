ba797f8d2758cfc2470bc068d4d0761b2e3baac7
wonabru <wonabru@wonabru-VirtualBox.(none)>
2014-01-11 02:56:26 +0100

    Kubuntu Experimental 32 bit

--- Q-qt-build-desktop-Qt_4_8_1_in_PATH__System__Debug/build/build.h
+++ Q-qt-build-desktop-Qt_4_8_1_in_PATH__System__Debug/build/build.h
@@ -1,2 +1,2 @@
 // No build information available
-#define BUILD_DATE ""
+#define BUILD_DATE "2014-01-10 22:57:47 +0100"
--- Q-qt-build-desktop-Qt_4_8_1_in_PATH__System__Debug/build/ui_optionsdialog.h
+++ Q-qt-build-desktop-Qt_4_8_1_in_PATH__System__Debug/build/ui_optionsdialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'optionsdialog.ui'
 **
-** Created: Fri Jan 10 22:16:54 2014
+** Created: Fri Jan 10 23:00:27 2014
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
