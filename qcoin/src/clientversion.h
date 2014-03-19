b6a4fb318cb38c9c92738e0d458df0301041cd26
wonabru <q@wonabru.com>
2014-03-19 23:18:24 +0100

    set version to 0.9.9.9

--- qcoin/src/clientversion.h
+++ qcoin/src/clientversion.h
@@ -6,10 +6,10 @@
 //
 
 // These need to be macros, as version.cpp's and PLM.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR       1
-#define CLIENT_VERSION_MINOR       6
-#define CLIENT_VERSION_REVISION    6
-#define CLIENT_VERSION_BUILD       6
+#define CLIENT_VERSION_MAJOR       0
+#define CLIENT_VERSION_MINOR       9
+#define CLIENT_VERSION_REVISION    9
+#define CLIENT_VERSION_BUILD       9
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
