688d71d9e43edfb0b5d9828a56d0f4b0e6ab1238
wonabru <q@wonabru.com>
2014-04-09 17:28:27 +0200

    some fixes

--- qcoin/src/qt/editaddressdialog.cpp
+++ qcoin/src/qt/editaddressdialog.cpp
@@ -36,7 +36,7 @@ EditAddressDialog::EditAddressDialog(Mode mode, QWidget *parent) :
     case EditReceivingAddress:
         setWindowTitle(tr("Put your name"));
         ui->addressEdit->setEnabled(true);
-        ui->labelEdit->setEnabled(false);
+        ui->labelEdit->setEnabled(true);
         break;
     case EditSendingAddress:
         setWindowTitle(tr("Names registered in PLM Network"));
