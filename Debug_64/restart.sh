41f0ba03aaf9efa6e2aa2fcbb85765eb111fa99d
wonabru <q@wonabru.com>
2014-04-17 17:58:41 +0200

    Release

--- Debug_64/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir ~/.PLMBackup
-cp ~/.PLM/myq.dat ~/.PLMBackup
-rm -rf ~/.PLM/[a-k]*
-rm -rf ~/.PLM/[n-z]*
---
+++ Release_64/build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2014-04-17 11:34:48 +0200"
